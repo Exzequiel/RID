89799a3dec8f19cf5c3d19d45301947933df0b3d
Exzequiel <exzequiel.lopez@gmail.com>
2017-09-23 11:51:26 -0600

    Index Initial

---
+++ RID/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RID.Startup))]
+namespace RID
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
